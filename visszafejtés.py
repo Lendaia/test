5bbea83180e9df263e069eff2907869d78e0788b
Mirai <92840672+Lendaia@users.noreply.github.com>
2024-07-31 18:39:40 +0200

    üzenetek visszafejtése
    folyamatban

---
+++ visszafejtés.py
@@ -0,0 +1,10 @@
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+words = []
+with open("words.txt", "r") as bemenet:
+    for i in bemenet:
+        words.append(i.strip())
+
+def kulcs(uzenet1, uzenet2, szo):
+    lehetoseg = ""
+
+    return lehetoseg

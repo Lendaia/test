232e6553c9acc2cb711ef88ce48352e65f1a007d
Székely-Bende Fruzsina <szbfru@gmail.com>
2024-08-03 23:35:24 +0200

    opcionális részlet

--- rejtjelezes.py
+++ rejtjelezes.py
@@ -1,6 +1,22 @@
-abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-uzenet = "helloworld"
+import random
+
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+uzenet = "curiosity killed the cat"
+
+# uzenet = input("üzenet: ")
+
 kulcs = "abcdefgijkl"
+
+# Amennyiben random generált kulcsot szeretnénk generálni, ez az egyszerű algoritmus használható
+def randomkulcs(hossz, keszlet):
+    ujkulcs = ""
+    for i in range(len(hossz) + random.randint(0, 50)):
+        ujkulcs += random.choice(keszlet)
+
+    return ujkulcs
+
+
+# kulcs = randomkulcs(uzenet, abc)
 
 
 def kod(titkositando, karakter):
@@ -20,5 +36,3 @@ for i in range(len(uzenet)):
     if szam > 26:
         szam = szam % 27
     titkositott += abc[szam]
-
-print(titkositott)

e0c81af9eff69921e2df299ea46fa50963f1b905
Székely-Bende Fruzsina <szbfru@gmail.com>
2024-08-12 00:40:53 +0200

    fejlesztés

--- visszafejtes.py
+++ visszafejtes.py
@@ -5,6 +5,7 @@ class uzenet:
     kod = []
     szavak = []
     betu = []
+    szint = 0
 
     def __init__(self, tartalom, kod):
         self.tartalom = tartalom
@@ -64,64 +65,100 @@ def kulcs(szo, keszlet, proba):
     db = 0
     aktualis = uzenet1.kod
     aktualisb = uzenet2.kod
-    problema = []
-    kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, problema)
+    flag = True
+    fejtett = ""
+    kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, flag, fejtett)
 
-def kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, problema):
+def kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, flag, fejtett):
     szokod = kodra(szo)
 
     while len(lehetoseg.kod) < hossz:
-        fejtett = ""
+        if flag:
+            fejtett = ""
+            for i in range(len(szo)):
+                szam = aktualis[i] - szokod[i]
+                if szam < 0:
+                    szam = (26 - szokod[i]) + aktualis[i] + 1
+                lehetoseg.kod.append(szam)
 
-        for i in range(len(szo)):
-            szam = aktualis[i] - szokod[i]
-            if szam < 0:
-                szam = (26 - szokod[i]) + aktualis[i] + 1
-            lehetoseg.kod.append(szam)
+            for i in range(len(lehetoseg.kod)):
+                szam = aktualisb[i] - lehetoseg.kod[i]
+                if szam < 0:
+                    szam = (26 - lehetoseg.kod[i]) + aktualisb[i] + 1
+                fejtett += keszlet[szam]
 
-        for i in range(len(lehetoseg.kod)):
-            szam = aktualisb[i] - lehetoseg.kod[i]
-            if szam < 0:
-                szam = (26 - lehetoseg.kod[i]) + aktualisb[i] + 1
-            fejtett += keszlet[szam]
-
-        for i in lehetoseg.kod:
-            lehetoseg.tartalom += abc[i]
-        lehetoseg.kod = []
+            for i in lehetoseg.kod:
+                lehetoseg.tartalom += abc[i]
+            lehetoseg.kod = []
+        else:
+            proba += 1
 
         if egyezik(fejtett):
-            if db == 0:
-                uzenet2.betu.append(fejtett)
-                uzenet2.szavak.append(egyezik(fejtett))
-                szo = uzenet2.szavak[-1][proba][len(fejtett):] + " "
-                szokod = kodra(szo)
+            if flag:
+                if db == 0:
+                    uzenet2.betu.append(fejtett)
+                    uzenet2.szavak.append(egyezik(fejtett))
+                    szo = uzenet2.szavak[-1][proba][len(fejtett):] + " "
+                    szokod = kodra(szo)
 
-                uzenet2.fejtett += uzenet2.szavak[-1][proba] + " "
-                aktualis = uzenet2.kod[(len(uzenet2.fejtett) - len(szo)):len(uzenet2.fejtett)]
-                aktualisb = uzenet1.kod[len(uzenet1.fejtett):]
-                db = 1
+                    uzenet2.fejtett += uzenet2.szavak[-1][proba] + " "
+                    aktualis = uzenet2.kod[(len(uzenet2.fejtett) - len(szo)):len(uzenet2.fejtett)]
+                    aktualisb = uzenet1.kod[len(uzenet1.fejtett):]
+                    uzenet2.szint += 1
+                    db = 1
+                else:
+                    uzenet1.betu.append(fejtett)
+                    uzenet1.szavak.append(egyezik(fejtett))
+                    szo = uzenet1.szavak[-1][proba][len(fejtett):] + " "
+                    szokod = kodra(szo)
+
+                    uzenet1.fejtett += uzenet1.szavak[-1][proba] + " "
+                    aktualis = uzenet1.kod[(len(uzenet1.fejtett) - len(szo)):len(uzenet1.fejtett)]
+                    aktualisb = uzenet2.kod[len(uzenet2.fejtett):]
+                    uzenet1.szint += 1
+                    db = 0
+
             else:
-                uzenet1.betu.append(fejtett)
-                uzenet1.szavak.append(egyezik(fejtett))
-                szo = uzenet1.szavak[-1][proba][len(fejtett):] + " "
-                szokod = kodra(szo)
+                flag = True
+                if db == 0:
+                    szo = uzenet2.szavak[-1][proba][len(fejtett):] + " "
+                    szokod = kodra(szo)
+                    aktualis = uzenet1.kod[(len(uzenet1.fejtett) - len(szo)):len(uzenet1.fejtett)]
+                    aktualisb = uzenet2.kod[len(uzenet2.fejtett):]
+                    uzenet2.szint -= 1
 
-                uzenet1.fejtett += uzenet1.szavak[-1][proba] + " "
-                aktualis = uzenet1.kod[(len(uzenet1.fejtett) - len(szo)):len(uzenet1.fejtett)]
-                aktualisb = uzenet2.kod[len(uzenet2.fejtett):]
-                db = 0
+                else:
+                    szo = uzenet1.szavak[-1][proba][len(fejtett):] + " "
+                    szokod = kodra(szo)
+                    aktualis = uzenet2.kod[(len(uzenet2.fejtett) - len(szo)):len(uzenet2.fejtett)]
+                    aktualisb = uzenet1.kod[len(uzenet1.fejtett):]
+                    uzenet1.szint -= 1
 
-            kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, problema)
+            kulcs_priv(szo, keszlet, proba, aktualis, aktualisb, db, flag, fejtett)
+            flag = False
+            if db == 0:
+                counter = 0
+                for i in range(len(uzenet1.fejtett[:-1])):
+                    if uzenet1.fejtett[i] == " ":
+                        counter += 1
+                        if counter == len(uzenet1.betu):
+                            uzenet1.fejtett = uzenet1.fejtett[:i]
+                            break
+            else:
+                counter = 0
+                for i in range(len(uzenet2.fejtett[:-1])):
+                    if uzenet2.fejtett[i] == " ":
+                        counter += 1
+                        if counter == len(uzenet2.betu):
+                            uzenet2.fejtett = uzenet2.fejtett[:i+1]
+                            break
 
-
-            #proba += 1
-            #if db == 0:
-            #    szo = uzenet2.szavak[-1][proba][len(fejtett):] + " "
-            #else:
-            #    szo = uzenet1.szavak[-1][proba][len(fejtett):] + " "
+            if len(uzenet1.fejtett) > len(uzenet2.fejtett):
+                lehetoseg.tartalom = lehetoseg.tartalom[:len(uzenet1.fejtett)]
+            else:
+                lehetoseg.tartalom = lehetoseg.tartalom[:len(uzenet2.fejtett)]
 
         else:
-            #TODO: 'zuxy'-nál nem akarunk a call stack-ben 'bird'-ig visszalépni
             return
 
     return lehetoseg.tartalom
